7be39c89e6684a4bf7529af9fe19945b3f0c489f
Souradipta Saha <137396269+Souradipta3@users.noreply.github.com>
2025-01-21 00:34:04 +0530

    Create 3.java

---
+++ 1. Fundamentals/3.java
@@ -0,0 +1,17 @@
+// Assignment 1.3: Calculate Factorial of a Number (Using Scanner)
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        long factorial = 1;
+
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("Factorial of " + num + " is: " + factorial);
+    }
+}
+
+// Explanation: This program calculates the factorial of a number using a loop.
